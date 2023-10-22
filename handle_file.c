7d309b7647b5b06e235bc6dcef7ad5411b030b47
malakehab2003 <lokaehab2003@gmail.com>
2023-10-22 16:59:56 +0300

    solve error

--- handle_file.c
+++ handle_file.c
@@ -71,12 +71,7 @@ void handle_file(char *file_read)
 
 int choose_func(char *command, char *arg, int line, int is_stack)
 {
-	if (arg == NULL)
-	{
-		fprintf(stderr, "L%d: usage: push integer\n", line);
-		return (-1);
-	}
-	else if (strcmp(command, "push") == 0 && is_stack == 1)
+	if (strcmp(command, "push") == 0 && is_stack == 1)
 		return (add_stack_node(arg, line));
 	else if (strcmp(command, "push") == 0 && is_stack == 0)
 		return (add_queue_node(arg, line));
